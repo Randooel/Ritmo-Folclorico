d82f7e549a2a7a972634b52e7711eeae2a04eebb
Leonardo Barros <leonardo.alves.barros.1@gmail.com>
2025-05-05 01:01:15 -0300

    0.01 - Conductor Implemented
    PACKAGES
    - Imported Cinemachine, Tile Editor and 2D Sprite package
    
    IMPLEMENTED
    - Conductor Script (for tracking song's rythm)
    - Basic setup in First_Level scene

---
+++ Quem Danca um Conto/Assets/Scripts/Rythm Scripts/Conductor.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conductor : MonoBehaviour
+{
+    // READ ME: This script is responsible for keeping track the music's rythm and
+    // current position (in seconds)
+
+    [Header("Song Info")]
+    [SerializeField] private float songBpm;
+    [SerializeField] private float secPerBeat;
+
+    [Header("Song Position")]
+    // Current song position in seconds
+    public float songPosition;
+    // Current song position in beats
+    public float songPositionInBeats;
+    // How many seconds have passed since song started
+    public float dspSongTime;
+
+    [Header("Audio Source")]
+    public AudioSource musicSource;
+
+
+    void Start()
+    {
+        // Gets the AudioSource component
+        musicSource = GetComponent<AudioSource>();
+
+        // Calculate the number of seconds in each beat
+        secPerBeat = 60f / songBpm;
+
+        // Record the time when music started
+        dspSongTime = (float)AudioSettings.dspTime;
+
+        // Plays music
+        musicSource.Play();
+    }
+
+    
+    void Update()
+    {
+        // Determine how many seconds passed since song started playing
+        songPosition = (float)(AudioSettings.dspTime - dspSongTime);
+
+        // Determine how many beats passed since song started playing
+        songPositionInBeats = songPosition / secPerBeat;
+    }
+}
